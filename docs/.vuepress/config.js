64617880da4ea22acd70efd6050d0be3fb969836
1393844304 <1393844304@qq.com>
2020-11-14 13:49:53 +0800

    完成了大致的文档编写，会战，游戏、系统部分已经完成， 剩余图库功能没有完成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,26 +3,25 @@ module.exports = {
         logo: '/assets/img/五等分的牢饭.jpg',
         activeHeaderLinks: true,
         displayAllHeaders: true,
-        sidebar: [
-            {
+        sidebar: [{
                 title: '会战类',
                 path: '/clan/',
-                collapsable: false,
-                sidebarDepth: 1,
+                collapsable: true,
+                sidebarDepth: 2,
                 children: ['/clan/clan_work', '/clan/clan_main']
             },
             {
                 title: '娱乐类',
                 path: '/entertain/',
-                collapsable: false,
-                sidebarDepth: 1,
-                children: ['/entertain/game', '/entertain/old_gacha', '/entertain/new_gacha']
+                collapsable: true,
+                sidebarDepth: 2,
+                children: ['/entertain/game', '/entertain/old_gacha', '/entertain/new_gacha', '/entertain/watch_photo']
             },
             {
                 title: '系统类',
                 path: '/system/',
-                collapsable: false,
-                siderbarDepth: 1,
+                collapsable: true,
+                siderbarDepth: 2,
                 children: ['/system/sys']
             }
         ]
